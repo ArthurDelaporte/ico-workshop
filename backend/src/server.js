35b119ae7a9e5f03e30799889cd2968f5c072bad
arthurdelaporte <arthur.delaporte@eemi.com>
2025-01-19 02:18:49 +0100

    essai modif pour Render

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,5 +2,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+    console.log(`🚀 Serveur lancé sur https://ico-workshop.onrender.com`);
 });
